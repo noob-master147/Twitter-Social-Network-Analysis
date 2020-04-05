a1e712cc68ee381b8b7c03d3da15622aa624c102
Divyansh <divyanshkhandelwal147@gmail.com>
2020-04-05 23:21:59 +0530

    server Created

--- REST_API/main.go
+++ REST_API/main.go
@@ -7,9 +7,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Struct for books
+//Struct for Book
 type Book struct {
-	Id     string  `json:"id"`
+	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
 	Title  string  `json:"title"`
 	Author *Author `json:"author"`
@@ -24,11 +24,31 @@ type Author struct {
 func main() {
 	//create a router
 	r := mux.NewRouter()
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBooks).Methods("POST")
-	r.HandleFunc("/api/books{id}", updateBooks).Methods("PUT")
-	r.HandleFunc("/api/books{id}", deleteBooks).Methods("DELETE")
+	r.HandleFunc("/api/books/all", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/book/create", createBook).Methods("POST")
+	r.HandleFunc("/api/book/update/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/book/delete/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
 }
