f2deac1323be33d0c873006126df7476016a424a
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-03 21:42:50 +0530

    tweepy streamer

---
+++ tweepyStream.py
@@ -0,0 +1,26 @@
+from tweepy.streaming import StreamListner
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import CREDENTIALS
+
+
+class TweetListner(StreamListner):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == "__main__":
+    listner = TweetListner()
+    auth = OAuthHandler(CREDENTIALS.CONSUMER_KEY, CREDENTIALS.CONSUMER_SECRET)
+    auth.set_access_token(CREDENTIALS.ACCESS_TOKEN,
+                          CREDENTIALS.ACCESS_TOKEN_SECRET)
+
+    stream = Stream(auth, listner)
+
+    stream.filter(track=['github'])
