22608f4b9c9a8015ad38d9165849bc2496944307
Divyansh <divyanshkhandelwal147@gmail.com>
2020-05-13 15:03:03 +0530

    JsPlumb

---
+++ jsPlumb-Examples-master/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ExJsplumb from './ExJsplumb6'; // Change Example name
+
+class App extends React.Component{
+  render() {
+    return (<>
+          <ExJsplumb />
+      </>);
+ }
+} 
+export default App;
